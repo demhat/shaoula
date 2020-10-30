54165d55a57b0838e5b83264f0aca5596d48142a
Demhat ŞAHİN <37871199+demhatts@users.noreply.github.com>
2020-10-30 15:31:47 +0300

    v 1.0.8

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const { nextI18NextRewrites } = require('next-i18next/rewrites')
 const localeSubpaths = { tr: 'tr' }
 
 module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths
-  },
   i18n: {
     locales: ['en', 'tr'],
     defaultLocale: 'en'
+  },
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
+  publicRuntimeConfig: {
+    localeSubpaths
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
